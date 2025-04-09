c7add36f4c8a443092e6a8d098144ee5f70f2932
furkancoram <furkancoram@gmail.com>
2025-04-09 17:22:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Profile from './pages/Profile';
 import Register from './pages/Register';
+import Profile from './pages/Profile';         // Profil düzenleme sayfası
+import ProfilePage from './pages/ProfilePage'; // Twitter tarzı profil sayfası
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profil" element={<Profile />} />
         <Route path="/kayit" element={<Register />} />
-        {/* Giriş yap sayfası ekleyeceksek: <Route path="/giris" element={<Login />} /> */}
+        <Route path="/profil" element={<Profile />} />
+        <Route path="/profilim" element={<ProfilePage />} />
+        {/* Giriş sayfası ekleyeceğimizde buraya gelir */}
       </Routes>
     </BrowserRouter>
   );
